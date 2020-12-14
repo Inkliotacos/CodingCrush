f5eed795639f86f654d245f2043f6ae476ac51d4
Marion <meurantmarion@gmail.com>
2020-12-14 17:34:58 +0100

    home page

--- codingcrush-front/src/views/Home.vue
+++ codingcrush-front/src/views/Home.vue
@@ -1,9 +1,42 @@
 <template>
   <div>
-    <h1>Bonjour {{ username }} !</h1>
-    <p>{{ secretMessage }}</p>
-    <img :src="urlImage">
-    <input type="button" value="Logout" @click="logout" />
+    <b-container>
+      <h1>Bonjour {{ username }} !</h1>
+      <b-form-input
+        size="10%"
+        class="mr-sm-2"
+        placeholder="Rechercher"
+      ></b-form-input>
+      <b-icon-search></b-icon-search>
+      <b-row cols-sm="5" cols-md="2">
+        <div class="col-8">
+                <b-row cols-sm="5" cols-md="1">
+          <h2 class="mt-3">Suggestion de profils</h2>
+          <b-list-group>
+            <div class="col">
+            <b-avatar :src="urlImage" size="200px"></b-avatar>
+            <h4>{{otherusers}}</h4>
+            <b-avatar :src="urlImage" size="200px"></b-avatar>
+            <h4>Nom 2</h4>
+            </div>
+             <div class="col">
+            <b-avatar :src="urlImage" size="200px"></b-avatar>
+            <h4>Nom 3</h4>
+            <b-avatar :src="urlImage" size="200px"></b-avatar>
+            <h4>Nom 4</h4>
+            </div>
+          </b-list-group>
+                </b-row>
+        </div>
+        <div class="col-4">
+          <h2 class="mt-3">Derniers quizz</h2>
+          <p>“Comment me connaître” par Esteban</p>
+          <p>“Ceci est un quiz” par Esteban</p>
+        </div>
+      </b-row>
+      <p>{{ secretMessage }}</p>
+      <input type="button" value="Logout" @click="logout" />
+    </b-container>
   </div>
 </template>
 
