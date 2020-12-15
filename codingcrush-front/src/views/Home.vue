9a7c137327f354bc85f667b9b2558c7d46dbfc4d
Marion <meurantmarion@gmail.com>
2020-12-15 17:46:17 +0100

    Link to profile

--- codingcrush-front/src/views/Home.vue
+++ codingcrush-front/src/views/Home.vue
@@ -14,13 +14,13 @@
           <h2 class="mt-3">Suggestion de profils</h2>
           <b-list-group>
             <div class="col" v-for="users in otherUsers" :key="users">
-              <b-avatar :src="users[0].profilimageurl" href='"/profile/" + users[0].id' size="200px"></b-avatar>
+              <b-avatar :src="users[0].profilimageurl" :href="'/profile/' + users[0].id" size="200px"></b-avatar>
               <h4>{{ users[0].username }}</h4>
-              <b-avatar :src="users[1].profilimageurl" size="200px"></b-avatar>
+              <b-avatar :src="users[1].profilimageurl" size="200px" :href="'/profile/' + users[1].id"></b-avatar>
               <h4>{{ users[1].username }}</h4>
-              <b-avatar :src="users[2].profilimageurl" size="200px"></b-avatar>
+              <b-avatar :src="users[2].profilimageurl" size="200px" :href="'/profile/' + users[2].id"></b-avatar>
               <h4>{{ users[2].username }}</h4>
-              <b-avatar :src="users[3].profilimageurl" size="200px"></b-avatar>
+              <b-avatar :src="users[3].profilimageurl" size="200px" :href="'/profile/' + users[3].id"></b-avatar>
               <h4>{{ users[3].username }}</h4>
             </div>
           </b-list-group>
